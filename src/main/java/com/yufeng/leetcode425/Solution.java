548db1926ef6934f2874d88283bf607e04b5914b
daiyu <jiangyufeng.0918@gmail.com>
2020-11-15 16:18:24 +0800

    字典树

---
+++ src/main/java/com/yufeng/leetcode425/Solution.java
@@ -0,0 +1,14 @@
+package com.yufeng.leetcode425;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public List<List<String>> wordSquares(String[] words) {
+
+        List<List<String>> ans = new LinkedList<>();
+
+
+        return ans;
+    }
+}
